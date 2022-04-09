9eadb0f284e3dd97d83b4237d13796f2b6311b35
Salman Mujtaba <salmanmujtaba@macbook.local>
2022-04-09 00:47:44 -0400

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,28 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { MatButtonModule } from '@angular/material/button';
+import { MatNativeDateModule } from '@angular/material/core';
+import { NgModule } from '@angular/core';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NoopAnimationsModule,
+    // BrowserModule,
+    FormsModule,
+    // HttpClientModule,
+    MatNativeDateModule,
+    // MaterialExampleModule,
+    ReactiveFormsModule,
+    // MaterialModule,
+    // MatCardModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
